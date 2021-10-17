c980840b69d8fd88c2d4f456d21a1ae8c68e30a0
Mariano Moran <mariano.moran@correo.unimet.edu.ve>
2021-10-17 17:28:30 -0400

    Javascript arreglado

--- main.js
+++ main.js
@@ -1,44 +1,3 @@
-window.onload = function () {
-  const img = [
-    "./img/LOGO_HOJALDRE_BAKERY.jpg",
-    "./img/Logo_Playmor1.png",
-    "./img/zkygo_black.png",
-  ];
-
-  const intervalo_img = 3000;
-  let actual_position = 0;
-  let $img = document.querySelector("#img");
-  let intervalo;
-
-  function next_img() {
-    if (actual_position >= img.length - 1) {
-      actual_position = 0;
-    } else {
-      actual_position++;
-    }
-    render_img();
-  }
-  function last_img() {
-    if (actual_position <= 0) {
-      actual_position = img.length - 1;
-    } else {
-      actual_position--;
-    }
-    render_img();
-  }
-
-  function render_img() {
-    $img.style.backgroundImage = `url(${img[actual_position]}`;
-  }
-
-  function carousel() {
-    intervalo = setInterval(next_img, intervalo_img);
-  }
-
-  render_img();
-  carousel();
-};
-
 const validatedEmail = (email) => {
   return /^[a-zA-Z0-9!#$%&'*+/=?¿^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(
     email
@@ -98,7 +57,46 @@ const submitForm = (e) => {
     alert("Nombre no valido");
   }
 };
+
 window.onload = function () {
+  const img = [
+    "./img/LOGO_HOJALDRE_BAKERY.jpg",
+    "./img/Logo_Playmor1.png",
+    "./img/zkygo_black.png",
+  ];
+
+  const intervalo_img = 3000;
+  let actual_position = 0;
+  let $img = document.querySelector("#img");
+  let intervalo;
+
+  function next_img() {
+    if (actual_position >= img.length - 1) {
+      actual_position = 0;
+    } else {
+      actual_position++;
+    }
+    render_img();
+  }
+  function last_img() {
+    if (actual_position <= 0) {
+      actual_position = img.length - 1;
+    } else {
+      actual_position--;
+    }
+    render_img();
+  }
+
+  function render_img() {
+    $img.style.backgroundImage = `url(${img[actual_position]}`;
+  }
+
+  function carousel() {
+    intervalo = setInterval(next_img, intervalo_img);
+  }
+
+  render_img();
+  carousel();
   const loadSkills = () => {
     const valores = document.getElementById("valores");
     const nameSkills = document.getElementById("name-skills");
