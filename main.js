662c5504f7a6db396a4a9b8020d49be686bb5d12
Mariano Moran <mariano.moran@correo.unimet.edu.ve>
2021-10-17 10:58:42 -0400

    Carousel y navbar disenados

---
+++ main.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+  const img = [
+    "./img/Polarcitaenhielo.jpg",
+    "./img/polarcitafondoneon.jpg",
+    "./img/Polarcitamesa.jpg",
+  ];
+
+  const intervalo_img = 3000;
+  let actual_position = 0;
+  let $img = document.querySelector("#img");
+  let intervalo;
+
+  function next_img() {
+    if (actual_position >= img.length - 1) {
+      actual_position = 0;
+    } else {
+      actual_position++;
+    }
+    render_img();
+  }
+  function last_img() {
+    if (actual_position <= 0) {
+      actual_position = img.length - 1;
+    } else {
+      actual_position--;
+    }
+    render_img();
+  }
+
+  function render_img() {
+    $img.style.backgroundImage = `url(${img[actual_position]}`;
+  }
+
+  function carousel() {
+    intervalo = setInterval(next_img, intervalo_img);
+  }
+
+  render_img();
+  carousel();
+};
